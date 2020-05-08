6885e80d1188b37c6867b2537fc48f7ffeff105d
Hyeshin Chu <hyeshin0511@gmail.com>
2020-05-08 18:06:08 +0000

    ff

--- config/routes.rb
+++ config/routes.rb
@@ -7,36 +7,16 @@ Rails.application.routes.draw do
 end
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-=======
 
   get 'farm' => 'posts#farm'
 
->>>>>>> 3030255d913c2e9eea0db32dc3600d1f57685a22
-
-
   post "posts/sell"
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3030255d913c2e9eea0db32dc3600d1f57685a22
   post 'posts/:post_id/likes' => 'likes#create'
   root 'posts#index'
  
   get '/recipe' => 'posts#recipe'
 
   get '/sell' => 'posts#sell'
-<<<<<<< HEAD
-  root 'posts#index'
- 
-  get '/recipe' => 'posts#recipe'
-=======
-
   get '/index' => 'posts#index'
-
->>>>>>> 3030255d913c2e9eea0db32dc3600d1f57685a22
-
-  get '/sell' => 'posts#sell'
-  get '/farm' => 'posts#farm'
 end
