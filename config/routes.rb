81a539abf6acd0b0903f1bd35c53adccbf98f369
Hyeshin Chu <hyeshin0511@gmail.com>
2020-05-08 14:50:46 +0000

    recipe page edition

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   post 'posts/:post_id/likes' => 'likes#create'
   root 'posts#index'
+  
+  get '/recipe' => 'posts#recipe'
 end
