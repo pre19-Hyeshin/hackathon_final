1352fa1b3d4406e0774806673efbb080f1ab5640
Ubuntu <ubuntu@ip-172-31-39-53.ec2.internal>
2020-05-08 15:30:15 +0000

    nothing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,4 +76,9 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :body)
     end
+    
+    def farm
+    end
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :farms
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/farm' => 'posts#farm'
   post 'posts/:post_id/likes' => 'likes#create'
   root 'posts#index'
+  
+  
+  
 end
